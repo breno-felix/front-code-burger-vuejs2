0e58fa962d6b0ebe3152ab15af9c7f74f72411db
Breno Felix <brenodev.felix@gmail.com>
2023-06-27 17:33:26 -0300

    :wrench:	chore: improve global styles.

--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,15 @@ export default {
 
 
 <style>
-@import "./styles/reset.css";
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    /* font-size: 100%;
+    vertical-align: baseline; */
+}
 </style>
