f9c87d3d66b410bc5330c250d8ec18c01a4edee5
Breno Felix <brenodev.felix@gmail.com>
2023-10-22 12:56:09 -0300

    :sparkles:	feat: create DefaultButton component.

---
+++ src/components/DefaultButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <b-button class="submit-button w-100" pill type="submit" :style="buttonStyle">
+            {{ buttonText }}
+        </b-button>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'DefaultButton',
+    props: {
+        backgroundColor: {
+            type: String,
+            default: '#9758A6'
+        },
+        minWidth: {
+            type: String,
+            default: '120px'
+        },
+        buttonText: {
+            type: String,
+        },
+    },
+    computed: {
+        buttonStyle() {
+            return {
+                backgroundColor: this.backgroundColor,
+                minWidth: this.minWidth,
+                border: 'none',
+            };
+        }
+    },
+};
+</script>
+  
+<style scoped>
+.submit-button:hover {
+    opacity: 0.8;
+}
+
+.submit-button:active {
+    opacity: 0.6;
+}
+</style>
