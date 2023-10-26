5769d6dcab3098e28e589a6c0f6cd430c43bf591
Breno Felix <brenodev.felix@gmail.com>
2023-10-26 07:52:51 -0300

    :wrench:	chore: add store to main.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import store from './store';
 import VueLidate from 'vuelidate'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
