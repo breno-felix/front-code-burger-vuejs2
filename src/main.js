01ab124acacdcb8011e4514513fb48a1b0c52ea4
Breno Felix <brenodev.felix@gmail.com>
2023-10-28 12:43:12 -0300

    :wrench:	chore: config vuejs-logger.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router';
-import store from './store';
-import VueLidate from 'vuelidate'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueLidate from "vuelidate";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueLogger from "vuejs-logger";
+import options from "./plugins/vue-logger";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(VueLidate)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(VueLidate);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(VueLogger, options);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
