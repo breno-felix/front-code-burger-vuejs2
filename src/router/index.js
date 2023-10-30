39cee17a3c2facf6402bfeb0e8e06cb67089f072
Breno Felix <brenodev.felix@gmail.com>
2023-10-30 18:23:32 -0300

    :wrench:	chore: config user routes.

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,11 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import authRoutes from './auth';
+import userRoutes from './user';
 
 const routes = [
   ...authRoutes,
+  ...userRoutes
 ];
 
 const router = new VueRouter({
