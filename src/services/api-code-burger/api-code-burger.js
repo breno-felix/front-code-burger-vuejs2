65eb8b8cd89e8ea76e6cf4949784dca016a291c5
Breno Felix <brenodev.felix@gmail.com>
2023-11-01 20:54:41 -0300

    :bug:	fix: token null.

--- src/services/api-code-burger/api-code-burger.js
+++ src/services/api-code-burger/api-code-burger.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
 const baseURL = process.env.VUE_APP_BASE_URL;
-const token = localStorage.getItem("token");
 
 const api = axios.create({
   baseURL,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 });
 
 export default api;
