7ff14a1412f7918348e4b38f3f90e0cf60e313cf
Breno Felix <brenodev.felix@gmail.com>
2023-11-06 20:48:43 -0300

    :wrench:	chore: config category store.

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from "vuex";
 
 import auth from "./modules/auth";
 import user from "./modules/user";
+import category from "./modules/category";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         auth,
-        user
+        user,
+        category
     }
 })
