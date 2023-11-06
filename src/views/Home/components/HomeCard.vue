dd05dafd7b95c4c1d50b31ff5e355c26cd3639d8
Breno Felix <brenodev.felix@gmail.com>
2023-11-06 20:51:28 -0300

    :sparkles:	feat: create HomeCard component.

---
+++ src/views/Home/components/HomeCard.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <b-card no-body border-variant="light" tag="button" style="max-width: 20rem;" class="p-0">
+            <b-card-img class="mb-3 card-img" :src="src" alt="category image" bottom></b-card-img>
+            <DefaultButton :buttonText="buttonText" :pill="pill" :width="width"></DefaultButton>
+        </b-card>
+    </div>
+</template>
+
+<script>
+import DefaultButton from '@/components/DefaultButton.vue'
+
+export default {
+    name: "HomeCard",
+    props: {
+        src: {
+            type: String,
+        },
+        buttonText: {
+            type: String,
+        },
+        pill: {
+            type: Boolean,
+            default: false
+        },
+        width: {
+            type: String,
+            default: "100%"
+        }
+    },
+    components: {
+        DefaultButton
+    },
+}
+</script>
+
+<style>
+.card-img {
+    max-width: 230px;
+    border-radius: 10px;
+}
+
+.card {
+    background-color: #f1f1f1;
+}
+</style>
