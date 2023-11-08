7477ad0b95d88934e1b6850aa12f6a01606cd060
Breno Felix <brenodev.felix@gmail.com>
2023-11-07 21:00:35 -0300

    :ok_hand:	style: add scoped option to style.

--- src/views/Home/components/CategoryCard.vue
+++ src/views/Home/components/CategoryCard.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .card-img {
     width: 230px;
     height: 230px;
--- src/views/Home/components/OfferCard.vue
+++ src/views/Home/components/OfferCard.vue
@@ -45,7 +45,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .card-img {
     width: 230px;
     height: 230px;
