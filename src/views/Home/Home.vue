b6bc4b26d23580bbbd9b2f158aa5636add9b36bc
Breno Felix <brenodev.felix@gmail.com>
2023-11-07 21:02:30 -0300

    :sparkles:	feat: add OfferCarousel component.

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,32 +1,44 @@
 <template>
   <b-container fluid class="h-100 w-100 p-0">
     <b-skeleton-img v-show="loadImg" no-aspect height="450px"></b-skeleton-img>
-    <b-img v-show="!loadImg" @load="imageLoaded" class="w-100" :src="require('../../assets/home-logo.svg')" fluid alt="image logo"></b-img>
+    <b-img v-show="!loadImg" @load="imageLoaded" class="w-100" :src="require('../../assets/home-logo.svg')" fluid
+      alt="image logo"></b-img>
 
-    <b-row class="container-category">
+    <b-row class="container-category pb-5">
       <b-col class="p-0">
-        <b-img class="my-5 w-25 category-img" :src="require('../../assets/CATEGORIAS.png')" center
-          alt="image category name"></b-img>
-        <HomeCarousel :categories="categories" :loading="loading"></HomeCarousel>
+        <b-img class="my-5 category-img" :src="require('../../assets/CATEGORIAS.png')" center
+          alt="category name image"></b-img>
+        <CategoryCarousel :items="categories" :loading="loading"></CategoryCarousel>
+      </b-col>
+    </b-row>
+
+    <b-row class="container-product pb-5">
+      <b-col class="p-0">
+        <b-img class="my-5 offer-img" :src="require('../../assets/OFERTAS.png')" center
+          alt="offer name image"></b-img>
+        <OfferCarousel :items="productsOffer" :loading="loading"></OfferCarousel>
       </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import HomeCarousel from './components/HomeCarousel.vue';
+import CategoryCarousel from './components/CategoryCarousel'
+import OfferCarousel from './components/OfferCarousel'
 import ToastMixin from '@/mixins/toastMixin.js';
 
 export default {
   name: "Home",
   mixins: [ToastMixin],
   components: {
-    HomeCarousel
+    CategoryCarousel,
+    OfferCarousel
   },
   async mounted() {
     try {
       this.loading = true
       this.categories = await this.$store.dispatch('listCategories');
+      this.productsOffer = await this.$store.dispatch('listProductsOffer');
       this.loading = false
     } catch (error) {
       this.makeToast(error, 'Mensagem de Erro!', 'warning')
@@ -37,13 +49,13 @@ export default {
       loading: true,
       loadImg: true,
       categories: [],
+      productsOffer: [],
     }
   },
 
   methods: {
     imageLoaded() {
       this.loadImg = false;
-      console.log("Imagem carregada");
     },
   },
 };
@@ -55,6 +67,16 @@ export default {
 }
 
 .category-img {
+  width: 25%;
   min-width: 200px;
+}
+
+.container-product {
+  background-color: white;
+}
+
+.offer-img {
+  width: 18%;
+  min-width: 145px;
 }
 </style>
