e63e00c8f2619ac18c3412e2c26b01c74b78dfeb
Breno Felix <brenodev.felix@gmail.com>
2023-11-09 07:50:33 -0300

    :wrench:	chore: add products route to app routes.

--- src/router/app.js
+++ src/router/app.js
@@ -1,10 +1,16 @@
 const Home = () => import('@/views/Home/Home.vue');
+const Products = () => import('@/views/Products/Products.vue');
 
 const appRoutes = [
   {
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: Products
   }
 ];
 
