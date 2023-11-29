1bb4aceab52a50ed44f7d0f7b72404b9b64a301c
Breno Felix <brenodev.felix@gmail.com>
2023-11-29 18:51:22 -0300

    :wrench:	chore: add cart route to app routes.

--- src/router/app.js
+++ src/router/app.js
@@ -1,5 +1,12 @@
 const Home = () => import('@/views/Home/Home.vue');
 const Products = () => import('@/views/Products/Products.vue');
+const Cart = () => import('@/views/Cart/Cart.vue');
+
+const requireNonEmptyCart = (to, from, next) => {
+  localStorage.getItem("cartProducts")
+    ? next()
+    : next({ name: "products", params: { cartProducts: 'empty' } });
+};
 
 const appRoutes = [
   {
@@ -11,6 +18,12 @@ const appRoutes = [
     path: '/products/:categoryId?',
     name: 'products',
     component: Products
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart,
+    beforeEnter: requireNonEmptyCart
   }
 ];
 
