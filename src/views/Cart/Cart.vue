50182a3f7e19936b40128dbf900e8de3b9c71f23
Breno Felix <brenodev.felix@gmail.com>
2023-11-30 21:15:12 -0300

    :sparkles: feat: add CartResume component and DefaultButton component for call submitOrder function.

--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -3,28 +3,59 @@
     <b-skeleton-img v-show="loadImg" no-aspect height="450px"></b-skeleton-img>
     <b-img v-show="!loadImg" @load="imageLoaded" class="w-100" :src="require('../../assets/cart-logo.svg')" fluid
       alt="image logo"></b-img>
-
-    <CartTable></CartTable>
+    <b-container class="py-5" fluid="lg">
+      <b-row>
+        <b-col order="1">
+          <CartTable></CartTable>
+        </b-col>
+        <b-col order-md="2">
+          <CartResume></CartResume>
+          <DefaultButton :buttonText="'Finalizar pedido'" :width="'100%'" class="mt-3" @buttonClick="submitOrder" :loading="loading"></DefaultButton>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-container>
 </template>
 
 <script>
 import CartTable from './components/CartTable.vue'
+import CartResume from './components/CartResume.vue'
+import DefaultButton from '@/components/DefaultButton.vue'
+import ToastMixin from '@/mixins/toastMixin.js';
 
 export default {
   name: "Cart",
+  mixins: [ToastMixin],
   components: {
-    CartTable
+    CartTable,
+    CartResume,
+    DefaultButton
   },
   data() {
     return {
       loadImg: true,
+      loading: false
     }
   },
   methods: {
     imageLoaded() {
       this.loadImg = false;
     },
+    submitOrder() {
+      this.loading = true
+      this.$store.dispatch('submitOrder')
+        .then((message) => {
+          this.makeToast(message, 'Tudo certo!', 'success');
+          setTimeout(() => {
+            this.$router.push('/products');
+            this.$store.dispatch('clearCart');
+          }, 4000)
+        }).catch(error => {
+          this.makeToast(error, 'Mensagem de Erro!', 'warning')
+        }).finally(() => {
+          this.loading = false
+        })
+    }
   },
 };
 </script>
