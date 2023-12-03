07a0ea63c7b27e13b1f28a9a7c6eb24b66149a94
Breno Felix <brenodev.felix@gmail.com>
2023-12-03 11:44:10 -0300

    :sparkles:	feat: add modal that removeProductFromCart.

--- src/views/Cart/Cart.vue
+++ src/views/Cart/Cart.vue
@@ -6,14 +6,41 @@
     <b-container class="py-5" fluid="lg">
       <b-row>
         <b-col order="1">
-          <CartTable></CartTable>
+          <CartTable @showRemoveProductModal="modalShowHandler"></CartTable>
         </b-col>
         <b-col order-md="2">
           <CartResume></CartResume>
-          <DefaultButton :buttonText="'Finalizar pedido'" :width="'100%'" class="mt-3" @buttonClick="submitOrder" :loading="loading"></DefaultButton>
+          <DefaultButton :buttonText="'Finalizar pedido'" :width="'100%'" class="mt-3" @buttonClick="submitOrder"
+            :loading="loading"></DefaultButton>
         </b-col>
       </b-row>
     </b-container>
+
+    <b-modal v-model="modalShow" title="Deletar produto" ok-only>
+      <b-container>
+        <b-row>
+          <p> Você tem certeza que deseja deletar este produto? </p>
+        </b-row>
+        <b-row>
+          <b-col cols="5" class="pr-0">
+            <b-card-img class="modal-img" :src="productToRemove.urlPath" :alt="'modal image'"></b-card-img>
+          </b-col>
+          <b-col class="pl-0">
+            <p><b>{{ productToRemove.name }}</b></p>
+            <p>{{ formatCurrency(productToRemove.price) }}</p>
+          </b-col>
+        </b-row>
+
+      </b-container>
+      <template #modal-footer="{ cancel }">
+        <b-button size="sm" variant="success" @click="removeProductFromCart(productToRemove._id)">
+          Sim
+        </b-button>
+        <b-button size="sm" variant="danger" @click="cancel()">
+          Não
+        </b-button>
+      </template>
+    </b-modal>
   </b-container>
 </template>
 
@@ -22,6 +49,7 @@ import CartTable from './components/CartTable.vue'
 import CartResume from './components/CartResume.vue'
 import DefaultButton from '@/components/DefaultButton.vue'
 import ToastMixin from '@/mixins/toastMixin.js';
+import formatCurrency from "@/utils/formatCurrency";
 
 export default {
   name: "Cart",
@@ -34,7 +62,10 @@ export default {
   data() {
     return {
       loadImg: true,
-      loading: false
+      loading: false,
+      modalShow: false,
+      productToRemove: {},
+      quantityProductCart: null
     }
   },
   methods: {
@@ -55,7 +86,23 @@ export default {
         }).finally(() => {
           this.loading = false
         })
-    }
+    },
+    modalShowHandler({ productToRemove, quantityProductCart }) {
+      this.productToRemove = productToRemove;
+      this.quantityProductCart = quantityProductCart
+      this.modalShow = true;
+    },
+    removeProductFromCart(productId) {
+      this.$store.dispatch('removeProductFromCart', productId);
+      this.makeToast('Produto removido com sucesso', 'Tudo certo!', 'success');
+      this.modalShow = false;
+      if (this.quantityProductCart === 1) {
+        this.$router.push({ name: 'products', params: { cartProducts: 'empty' } });
+      }
+    },
+    formatCurrency(value) {
+      return formatCurrency(value);
+    },
   },
 };
 </script>
@@ -65,5 +112,11 @@ export default {
   background-color: #EFEFEF;
   min-height: 100%;
   max-height: max-content;
+}
+
+.modal-img {
+  width: 150px;
+  height: 150px;
+  border-radius: 10px;
 }
 </style>
